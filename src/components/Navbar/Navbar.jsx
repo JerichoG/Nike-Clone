7b542243798d293fce67e09425dc68dda775298c
Jericho <edwardk753@gmail.com>
2021-03-26 08:24:33 +0700

    Add Cart and CartItem

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+import useStyles from './style';
+
+const Navbar = ({ totaLItems }) => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    return(
+        <>
+             <AppBar position="fixed" className={classes.appBar} color="inherit">
+                    <Toolbar>
+                        <Typography  component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+                            <img src={logo} alt="Nike" height="25px" className={classes.image} />
+                        </Typography>
+
+                        <div className={classes.grow}  />
+                        { location.pathname === '/' && (
+                            <div className={classes.button}>
+                                <IconButton  component={Link} to="/cart" color="inherit">
+                                    <Badge badgeContent={ totaLItems } color="error">
+                                        <ShoppingCart/>
+                                    </Badge>
+                                </IconButton> 
+                            </div>
+                        )}
+                          
+                    </Toolbar>
+             </AppBar>
+        </>
+    )
+}
+
+export default Navbar
